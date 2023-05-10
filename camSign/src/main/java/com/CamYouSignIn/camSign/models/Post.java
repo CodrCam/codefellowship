cd3161e21e9cf5524aae3a596aada6c0efa165ff
Cameron Griffin (He/Him) <ct.griffin7@gmail.com>
2023-05-09 23:55:04 -0700

    add post

---
+++ camSign/src/main/java/com/CamYouSignIn/camSign/models/Post.java
@@ -0,0 +1,64 @@
+package com.CamYouSignIn.camSign.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String body;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private ApplicationUser applicationUser;
+
+    public Post() {
+    }
+
+    public Post(String body, ApplicationUser applicationUser) {
+        this.body = body;
+        this.applicationUser = applicationUser;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    // Getters and setters for all fields
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public ApplicationUser getApplicationUser() {
+        return applicationUser;
+    }
+
+    public void setApplicationUser(ApplicationUser applicationUser) {
+        this.applicationUser = applicationUser;
+    }
+}
